b2366c1d5d9a4849271382b144fa231f8d9cf4fe
paterajm <134966807+paterajm@users.noreply.github.com>
2023-05-30 01:52:25 -0400

    Update JavaApplication4.java

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -15,6 +15,7 @@ public class JavaApplication4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        system.out.println("Helloooooo!")
     }
     
 }
